6bf789f76ea7f1f5e127568e970e5850cde8906e
EfsaneHesenova <160878199+EfsaneHesenova@users.noreply.github.com>
2024-10-10 11:04:34 +0400

    methodtask1 successfull

---
+++ Methodstask1/Methodstask1/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace Methods;
+
+    internal class Program
+{
+    static void Main(string[] args)
+    {
+        string text = "Hello my name is Afsana";
+        string searching = "name";
+        bool end = CustomCharAt(text, searching);
+        Console.WriteLine(end);
+    }
+        
+    public static bool CustomCharAt(string text, string searching)
+    {
+        for (int i = 0; i <= text.Length - searching.Length; i++)
+        {
+            int j = 0;
+            for(j = 0; j < searching.Length; j++)
+            {
+                if (text[i+j] != searching[j])
+                {
+                    break;
+                }
+                
+            }
+            if (j == searching.Length)
+            { return true; }
+        }
+        return false;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
