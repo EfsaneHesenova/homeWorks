612e363c18cf863e81c6914541ad437afbcd4cd6
EfsaneHesenova <160878199+EfsaneHesenova@users.noreply.github.com>
2024-10-13 22:09:22 +0400

    classtask successfully

---
+++ classtask/classtask/Animal.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classtask
+{
+    public class Animal
+    {
+        public string name;
+        public int age;
+
+        public void Eat()
+        { 
+            Console.WriteLine($"able to eat");
+        }
+    }
+    public class Dog : Animal
+    {
+
+        public bool hasTail;
+
+        public Dog(string name, int age, bool hasTail)
+        {
+            this.name = name;
+            this.age = age; 
+            this.hasTail = hasTail;
+
+        }
+        public void Bark() 
+        {
+            Console.WriteLine($"Dogs are Barking");
+        }
+
+        public void GetInfo()
+        {
+            Console.WriteLine($"New dogs name: {name} \nNew dogs age: {age} \nIs new dog has tail? : {hasTail}");
+        }
+    }
+    public class Bear : Animal
+    {
+        public bool isWild;
+
+
+        public Bear(string name, int age, bool isWild)
+        {
+            this.name = name;
+            this.age = age;
+            this.isWild = isWild;
+        }
+        public void GetInfo()
+        {
+            Console.WriteLine($"New Bears name: {name} \nNew Bears age: {age} \nIs new Bear is wild? : {isWild}");
+        }
+    }
+    public class Bird : Animal
+    {
+        public bool CanSwim;
+        public Bird(string name, int age, bool canSwim)
+        {
+            this.name = name;
+            this.age = age;
+            this.CanSwim = canSwim;
+        }
+        public void Fly()
+        { 
+            Console.WriteLine("Fly"); 
+        }
+        public void GetInfo()
+        {
+            Console.WriteLine($"New Birds name: {name} \nNew Birds age: {age} \nIs new Birds is wild? : {CanSwim}");
+        }
+    }
+}
---
+++ classtask/classtask/Program.cs
@@ -0,0 +1,83 @@
+﻿using System.Linq.Expressions;
+
+namespace classtask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string animalType;
+            while (true) 
+            {
+                Console.WriteLine("Heyvan daxil edin:");
+                animalType = Console.ReadLine();
+                if (animalType == "Dog" || animalType == "Bear" || animalType == "Bird")
+                {
+                    if (animalType == "Dog")
+                    {
+                        string dogName;
+                        int dogAge;
+                        bool hasTail;
+                        Console.Write($"What is your dog name: ");
+                        dogName = Console.ReadLine();
+                        Console.Write($"What is your dog age: ");
+                        dogAge = int.Parse(Console.ReadLine());
+                        Console.Write($"Is your dog has a tail: ");
+                        hasTail = bool.Parse(Console.ReadLine());
+
+
+                        Dog newDog = new Dog(dogName, dogAge, hasTail);
+                        
+                        newDog.GetInfo();
+                        newDog.Bark();
+                        newDog.Eat();
+                        break;
+                    }
+                    else if (animalType == "Bear")
+                    {
+                        String BearName;
+                        int BearAge;
+                        bool isWild;
+                        Console.Write($"What is your Bear name: ");
+                        BearName = Console.ReadLine();
+                        Console.Write($"What is your Bear age: ");
+                        BearAge = int.Parse(Console.ReadLine());
+                        Console.Write($"Is your Bear is a wild: ");
+                        isWild = bool.Parse(Console.ReadLine());
+
+
+                        Bear newBear = new Bear(BearName, BearAge, isWild);
+                        newBear.GetInfo();
+                        newBear.Eat();
+                        break;
+                    }
+                    else if (animalType == "Bird")
+                    {
+                        String BirdName;
+                        int BirdAge;
+                        bool canSwim;
+                        Console.Write($"What is your Bird name: ");
+                        BirdName = Console.ReadLine();
+                        Console.Write($"What is your Bird age: ");
+                        BirdAge = int.Parse(Console.ReadLine());
+                        Console.Write($"Is your Bird can a swim: ");
+                        canSwim = bool.Parse(Console.ReadLine());
+
+
+                        Bird newBird = new Bird(BirdName, BirdAge, canSwim);
+                        newBird.GetInfo();
+                        newBird.Fly();
+                        newBird.Eat();
+                        break;
+                    }
+                }
+                else 
+                {
+                    Console.WriteLine("Please select only dog,bear or bird");
+                    continue;
+                }
+
+            }
+        }
+    }
+}
