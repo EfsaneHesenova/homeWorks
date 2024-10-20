9e55cb7eb0e05a58dba13c399c52edadcc168d53
EfsaneHesenova <160878199+EfsaneHesenova@users.noreply.github.com>
2024-10-20 21:12:32 +0400

    Last version

---
+++ WeaponProject/WeaponProject/Models/Weapon.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.Design;
+
+namespace WeaponProject.Models
+{
+    internal class Weapon
+    {
+        public int CapacityBullet { get; set; }
+        private int _numBullet;
+        public int NumBullet
+        {
+            get
+            { 
+                return _numBullet;
+            }
+            set
+            {
+                
+                if (value >= 0)
+                {
+                    _numBullet = value;
+                }
+                else
+                {
+                    Console.WriteLine("sizin daraqda gulle bitib");
+                }
+            }
+        }
+        private string _shotMode;
+        public string ShotMode 
+        { 
+            get
+            {
+                return _shotMode;
+            }
+            set
+            {
+                if (value == "single" || value == "auto")
+                {
+                    _shotMode = value;
+                }
+                else
+                {
+                    Console.WriteLine("bu cur mod mumkun deyil");
+                }
+            }
+        }
+        public Weapon(int capacityBullet, int numBullet, string shotMode)
+        {
+            CapacityBullet = capacityBullet;
+            NumBullet = numBullet;
+            ShotMode = shotMode;
+        }
+        public void Shoot()
+        {
+            if (NumBullet > 0)
+            {
+                NumBullet--;
+                Console.WriteLine($"emeliyyat icra olundu 1 gulle atildi. {NumBullet} / {CapacityBullet} ");
+            }
+            else
+            {
+                Console.WriteLine("sizin daraqda gulle bitib");
+            }
+            
+        }
+        public void Fire()
+        {
+            if (ShotMode == "auto" && NumBullet > 0)
+            {
+                int startNumBullet = NumBullet;
+
+                Stopwatch fireModeTime = new Stopwatch();
+
+                fireModeTime.Start();
+
+                for (int i = 0; i < startNumBullet; i++)
+                {
+                    NumBullet--;
+                }
+                Thread.Sleep(2000);
+
+                fireModeTime = new Stopwatch();
+
+                Console.WriteLine($"{startNumBullet} eded gulle atildi. Proses {fireModeTime.ElapsedMilliseconds} millisaniye cekdi . Daraqda gulle sayi bitdi {NumBullet} / {CapacityBullet}");
+            }
+
+            else if (NumBullet <= 0)
+            {
+                Console.WriteLine("sizin daraqda gulle bitib");
+            }
+
+            else
+            {
+                Console.WriteLine("yalniz auto mod zamani ise dusur");
+            }
+        }
+        public void GetRemainBulletCount()
+        {
+            int need = CapacityBullet - NumBullet;
+            Console.WriteLine(need);
+        }
+        public void Reload()
+        {
+            
+            
+                int need = CapacityBullet - NumBullet;
+                NumBullet += need;
+                Console.WriteLine($" {need} gulle dolduruldu");
+           
+        }
+        public void ChangeFireMode()
+        {
+            if (ShotMode == "auto")
+            {
+                ShotMode = "single";
+                Console.WriteLine($"mod: {ShotMode}");
+            }
+            else if (ShotMode == "single" )
+            {
+                ShotMode = "auto";
+                Console.WriteLine($"mod: {ShotMode}");
+            }
+            else
+            { Console.WriteLine("bu cur mod mumkun deyil"); }
+            
+        }
+
+
+    }
+}
---
+++ WeaponProject/WeaponProject/Program.cs
@@ -0,0 +1,63 @@
+﻿using WeaponProject.Models;
+namespace WeaponProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int userCapacityBullet;
+            int userNumBullet;
+            string userShootMode;
+            Console.WriteLine("Gulle tutumu:");
+            userCapacityBullet = int.Parse(Console.ReadLine());
+            Console.WriteLine("Gulle sayi:");
+            userNumBullet = int.Parse(Console.ReadLine());
+            Console.WriteLine("mode:");
+            userShootMode = Console.ReadLine();
+            Weapon weapon = new Weapon(userCapacityBullet, userNumBullet, userShootMode);
+            bool isTrue = true;
+            while (isTrue)
+            {
+                Console.WriteLine("""
+                0 - İnformasiya almaq üçün
+                1 - Shoot metodu üçün
+                2 - Fire metodu üçün
+                3 - GetRemainBulletCount metodu üçün
+                4 - Reload metodu üçün
+                5 - ChangeFireMode metodu üçün
+                6 - Proqramdan dayandırmaq üçün
+
+                Zehmet olmasa secim edin:
+                """);
+                string choose = Console.ReadLine();
+
+                switch (choose)
+                {
+                    case "0":
+                        Console.WriteLine($"Sizin Silah atis proqrami  \nsilah tutumu: {weapon.CapacityBullet} \nsilahdaki gulle sayi: {weapon.NumBullet}  \nsizin atis mod: {weapon.ShotMode} ");
+                        break;
+                    case "1":
+                        weapon.Shoot();
+                        break;
+                    case "2":
+                        weapon.Fire();
+                        break;
+                    case "3":
+                        weapon.GetRemainBulletCount();
+                        break;
+                    case "4":
+                        weapon.Reload();
+                        break;
+                    case "5":
+                        weapon.ChangeFireMode();
+                        break;
+                    case "6":
+                            isTrue = false;
+                        break;
+
+                }
+            }
+        }
+    }
+}
