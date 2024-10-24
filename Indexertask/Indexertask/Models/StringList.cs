b542906a26f3d3cabcf3d847f3e8756c5a8bc415
EfsaneHesenova <160878199+EfsaneHesenova@users.noreply.github.com>
2024-10-24 23:44:06 +0400

    indexer task

---
+++ Indexertask/Indexertask/Models/StringList.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Indexertask.Models
+{
+    internal  class StringList
+    {
+        public  string[] cities;
+
+        public StringList()
+        {
+            cities = new string[0];
+        }
+        public void AddCity(string city)
+        {
+            Array.Resize(ref cities, cities.Length+1);
+            cities[^1] = city;
+        }
+        public int this[string cityName]
+        {
+            get
+            {
+                for (int i = 0; i < cities.Length; i++)
+                {
+                    if (cities[i] == cityName) { return i; }
+                }
+                return -1;
+            }
+        }
+    }
+    
+}
---
+++ Indexertask/Indexertask/Program.cs
@@ -0,0 +1,16 @@
+﻿using Indexertask.Models;
+
+namespace Indexertask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StringList cityObj = new StringList();
+            cityObj.AddCity("New York");
+            cityObj.AddCity("Tokyo");
+            cityObj.AddCity("Baku");
+            Console.WriteLine(cityObj["Baku"]); 
+        }
+    }
+}
