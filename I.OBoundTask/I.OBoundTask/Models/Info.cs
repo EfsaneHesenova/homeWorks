e50ac616a7f562832735f88123ed9f351d4fea4d
EfsaneHesenova <160878199+EfsaneHesenova@users.noreply.github.com>
2024-11-06 10:59:26 +0400

    Directory Task Added

---
+++ I.OBoundTask/I.OBoundTask/Models/Info.cs
@@ -0,0 +1,10 @@
+namespace DirectoryFiletask1.Models;
+
+public class Info
+{
+    public int UserId { get; set; }
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Body { get; set; }
+
+}
--- I.OBoundTask/I.OBoundTask/Program.cs
+++ I.OBoundTask/I.OBoundTask/Program.cs
@@ -1,47 +1,98 @@
-﻿using System.Diagnostics;
+﻿using DirectoryFiletask1.Models;
+using Newtonsoft.Json;
+using System.Text.Json.Nodes;
+using System.Text.Json.Serialization;
+
 
 namespace I.OBoundTask
+{
+    internal class Program
     {
-        internal class Program
+        static void Main(string[] args)
         {
-            static void Main(string[] args)
+            /*#region IO bound Main
+            Stopwatch st = Stopwatch.StartNew();
+            List<string> urls = new List<string>
             {
-                Stopwatch st = Stopwatch.StartNew();
-                List<string> urls = new List<string>
-                {
                 "https://www.w3schools.com/",
                 "https://www.youtube.com/",
                 "https://avtovagzal.az/page/tarifler"
+            };
+            SyncProgram(urls);
+            Console.WriteLine(st.ElapsedMilliseconds);
+            st.Restart();
+            ASyncProgram(urls);
+            Console.WriteLine(st.ElapsedMilliseconds);
 
-                };
-                SyncProgram(urls);
-                Console.WriteLine(st.ElapsedMilliseconds);
-                Console.WriteLine("--------------------------------------------------------------");
-                st.Restart();
+            #endregion*/
 
-                AsyncProgram(urls).Wait();
-                Console.WriteLine(st.ElapsedMilliseconds);
-
-            }
-            public static void SyncProgram(List<string> urls)
+            #region Directory/File Task Main
+            string url = "https://jsonplaceholder.typicode.com/posts";
+            string path = @"C:\Users\User\source\repos\I.OBoundTask\I.OBoundTask";
+            Directory.CreateDirectory(path + @"\Models");
+            Directory.CreateDirectory(path + @"\Data");
+            if (!File.Exists(path + @"\Data\jsonData.json"))
             {
-                HttpClient httpClient = new HttpClient();
-                foreach (string item in urls)
-                {
-                    httpClient.GetStringAsync(item).Result.ToString();
-                }
+                File.Create(path + @"\Data\jsonData.json");
             }
-            public static async Task AsyncProgram(List<string> urls)
+            else
             {
-                HttpClient httpClient = new HttpClient();
+                Console.WriteLine("Already exists");
+            }
 
-                List<Task<string>> tasks = new List<Task<string>>();
+            if (!File.Exists(path + @"\Models\Info.cs"))
+            {
+                File.Create(path + @"\Models\Info.cs");
+            }
+            else
+            {
+                Console.WriteLine("Already exists");
+            }
 
-                foreach (string item in urls)
-                {
-                    tasks.Add(httpClient.GetStringAsync(item));
-                }
-                await Task.WhenAll(tasks);
+            string data = AsyncProgramReturn(url).Result;
+
+            List<Info> datas = JsonConvert.DeserializeObject<List<Info>>(data);
+
+            string Serialazed = JsonConvert.SerializeObject(datas, Formatting.Indented);
+            using (StreamWriter sw = new StreamWriter(path + @"\Data\jsonData.json"))
+            {
+                sw.WriteLine(Serialazed);
+            }
+
+            #endregion 
+        }
+
+        #region Directory/File Task Method
+        static async Task<string> AsyncProgramReturn(string url)
+        {
+            HttpClient httpClient = new HttpClient();
+            string data = await httpClient.GetStringAsync(url);
+            return data;
+        }
+
+        #endregion
+
+        /*#region Io bound methods
+        public static void SyncProgram(List<string> urls)
+        {
+            HttpClient httpClient = new HttpClient();
+            foreach (string item in urls)
+            {
+                httpClient.GetStringAsync(item).Result.ToString();
             }
         }
+        public static async Task ASyncProgram(List<string> urls)
+        {
+            HttpClient httpClient = new HttpClient();
+            List<Task<string>> tasks = new List<Task<string>>();
+            foreach (string item in urls)
+            {
+                tasks.Add(httpClient.GetStringAsync(item));
+            }
+            await Task.WhenAll(tasks);
+        }
+        #endregion*/
+
+
     }
+}
